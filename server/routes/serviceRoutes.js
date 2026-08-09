da257e4a48eef2bc9474fc1309dae61275ccefd6
Rajeev <sivarajeev511@gmail.com>
2026-08-09 22:56:56 +0530

    Add serviceRoute file

---
+++ server/routes/serviceRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { getServices, getService, createService, updateService, deleteService } = require('../controllers/serviceController');
+const { protect, admin } = require('../middleware/auth');
+
+router.route('/')
+  .get(getServices)
+  .post(protect, admin, createService);
+
+router.route('/:id')
+  .get(getService)
+  .put(protect, admin, updateService)
+  .delete(protect, admin, deleteService);
+
+module.exports = router;
